6514f933c45da856e2746b003069457443e00f99
unknown <danyalj859@gmail.com>
2025-12-14 11:41:40 +0500

    Frontend Intergration

--- src/main/java/com/order/exception/OutOfStockException.java
+++ src/main/java/com/order/exception/OutOfStockException.java
@@ -1,6 +1,11 @@
 package com.order.exception;
 
 public class OutOfStockException extends RuntimeException{
+/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 public OutOfStockException(String msg) {
 	super(msg);
 }
